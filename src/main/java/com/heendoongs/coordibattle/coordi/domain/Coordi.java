7abe2fa69a9687d9cd465682b5c24072e6684ded
wlstnam <wlstnam99@gmail.com>
2024-07-25 16:59:44 +0900

    init settings

---
+++ src/main/java/com/heendoongs/coordibattle/coordi/domain/Coordi.java
@@ -0,0 +1,41 @@
+package com.heendoongs.coordibattle.coordi.domain;
+
+import com.heendoongs.coordibattle.battle.domain.Battle;
+import com.heendoongs.coordibattle.member.domain.Member;
+import com.heendoongs.coordibattle.member.domain.MemberCoordiVote;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Coordi {
+
+    @Id @GeneratedValue
+    @Column(name = "COORDI_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BATTLE_ID")
+    private Battle battle;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "COORDI_IMAGE")
+    private byte[] coordiImage;
+
+    @Column(name = "CREATED_DATE")
+    private LocalDate createdDate;
+
+    @OneToMany(mappedBy = "coordi")
+    List<MemberCoordiVote> memberCoordiVotes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "coordi")
+    List<CoordiClothes> coordiClothes = new ArrayList<>();
+}
---
+++ src/test/java/com/heendoongs/coordibattle/CoordibattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heendoongs.coordibattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordibattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
