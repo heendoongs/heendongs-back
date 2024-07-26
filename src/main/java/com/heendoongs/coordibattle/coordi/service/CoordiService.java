ac021eb6a5b2aa364f9006a2f7d974cf346ae996
wj0624 <wonjeong4538@gmail.com>
2024-07-26 17:57:58 +0900

    Chore: 코디 관련 파일 생성

---
+++ src/main/java/com/heendoongs/coordibattle/coordi/service/CoordiService.java
@@ -0,0 +1,17 @@
+package com.heendoongs.coordibattle.coordi.service;
+
+/**
+ * 코디 서비스
+ * @author 임원정
+ * @since 2024.07.26
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.07.26  	임원정       최초 생성
+ * </pre>
+ */
+
+public interface CoordiService {
+}
