5f039451d680929926ea2f1959eacd0f5dea4c35
hodupie <hjchocmt@gmail.com>
2024-08-07 19:22:21 +0900

    Fix: access Token 재발급 시간 변경

--- src/main/java/com/heendoongs/coordibattle/global/jwt/LoginFilter.java
+++ src/main/java/com/heendoongs/coordibattle/global/jwt/LoginFilter.java
@@ -106,7 +106,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String role = auth.getAuthority();
 
         //토큰 생성
-        String access = jwtUtil.createJwt("access", username, memberId, role, 60 * 60 * 10000L);
+        String access = jwtUtil.createJwt("access", username, memberId, role, 86400000L);
         String refresh = jwtUtil.createJwt("refresh", username, memberId, role, 86400000L);
 
         //응답 설정
@@ -123,8 +123,6 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
      */
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
         System.out.println("Authentication failed: {}" + failed.getMessage());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
